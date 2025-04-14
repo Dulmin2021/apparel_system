e81c67a819de2dd01f577035e5f5087f1c842bf6
Dulmin2021 <dulmin.edu@gmail.com>
2025-04-14 17:08:22 +0530

    New label created

--- dashboard.py
+++ dashboard.py
@@ -7,7 +7,10 @@ window.geometry('1270x668+0+0')
 window.resizable(0,0)
 window.config(bg='white')
 
-titleLabel =Label(window, text='CIB Apparel System')
+bg_Image=PhotoImage(file='assets/logo.png')
+titleLabel =Label(window, image=bg_Image, compound=RIGHT, text='     CIB Apparel System  ', font=('times new roman', 40, 'bold'), bg='#010c48', fg='white', anchor='w', padx=20)
+titleLabel.place(x=0, y=0, relwidth=1)
+
 
 
 window.mainloop()
