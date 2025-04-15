d85c37f1e4b2959cb2b22a207dcd57cdd0499e2b
Dulmin2021 <dulmin.edu@gmail.com>
2025-04-15 10:13:28 +0530

    Added Frames

--- dashboard.py
+++ dashboard.py
@@ -57,8 +57,78 @@ exit_icon=PhotoImage(file='assets/exit.png')
 exit_button=Button(leftFrame, image=exit_icon, compound=LEFT, text='  Exit', font=('times new roman', '20', 'bold'), anchor='w', padx=10)
 exit_button.pack(fill=X)
 
+##############################################################################################################
 
-######################################################################################################################
+
+
+##################################FRAMES################FRAMES################FRAMES##########################
+
+#Employee Frame
+emp_frame=Frame(window, bg='#2C3E50', bd=3, relief=RIDGE)
+emp_frame.place(x=400, y=125, height=170, width=280) 
+total_emp_icon=PhotoImage(file='')
+total_emp_icon_label=Label(emp_frame, image=total_emp_icon, bg='#2C3E50',)
+total_emp_icon_label.pack()
+
+total_emp_label=Label(emp_frame, text= 'Total Employees', bg='#2C3E50', fg='white', font=('times new roman', '15', 'bold'))
+total_emp_label.pack()
+total_emp_count_label=Label(emp_frame, text= '0', bg='#2C3E50', fg='white', font=('times new roman', '30', 'bold'))
+total_emp_count_label.pack()
+
+
+#Supplier Frame
+sup_frame=Frame(window, bg='#8E44AD', bd=3, relief=RIDGE)
+sup_frame.place(x=800, y=125, height=170, width=280) 
+total_sup_icon=PhotoImage(file='')
+total_sup_icon_label=Label(sup_frame, image=total_sup_icon, bg='#8E44AD',)
+total_sup_icon_label.pack()
+
+total_sup_label=Label(sup_frame, text= 'Total Suppliers', bg='#8E44AD', fg='white', font=('times new roman', '15', 'bold'))
+total_sup_label.pack()
+total_sup_count_label=Label(sup_frame, text= '0', bg='#8E44AD', fg='white', font=('times new roman', '30', 'bold'))
+total_sup_count_label.pack()
+
+
+#Category Frame
+cat_frame=Frame(window, bg='#27AE60', bd=3, relief=RIDGE)
+cat_frame.place(x=400, y=310, height=170, width=280) 
+total_cat_icon=PhotoImage(file='')
+total_cat_icon_label=Label(cat_frame, image=total_cat_icon, bg='#27AE60',)
+total_cat_icon_label.pack()
+
+total_cat_label=Label(cat_frame, text= 'Total Categories', bg='#27AE60', fg='white', font=('times new roman', '15', 'bold'))
+total_cat_label.pack()
+total_cat_count_label=Label(cat_frame, text= '0', bg='#27AE60', fg='white', font=('times new roman', '30', 'bold'))
+total_cat_count_label.pack()
+
+
+#Products Frame
+prod_frame=Frame(window, bg='#27AE60', bd=3, relief=RIDGE)
+prod_frame.place(x=800, y=310, height=170, width=280) 
+total_prod_icon=PhotoImage(file='')
+total_prod_icon_label=Label(prod_frame, image=total_prod_icon, bg='#27AE60',)
+total_prod_icon_label.pack()
+
+total_prod_label=Label(prod_frame, text= 'Total Products', bg='#27AE60', fg='white', font=('times new roman', '15', 'bold'))
+total_prod_label.pack()
+total_prod_count_label=Label(prod_frame, text= '0', bg='#27AE60', fg='white', font=('times new roman', '30', 'bold'))
+total_prod_count_label.pack()
+
+
+#Sales Frame
+sales_frame=Frame(window, bg='#E74C3C', bd=3, relief=RIDGE)
+sales_frame.place(x=600, y=495, height=170, width=280) 
+total_sales_icon=PhotoImage(file='')
+total_sales_icon_label=Label(sales_frame, image=total_sales_icon, bg='#E74C3C',)
+total_sales_icon_label.pack()
+
+total_sales_label=Label(sales_frame, text= 'Total Sales', bg='#E74C3C', fg='white', font=('times new roman', '15', 'bold'))
+total_sales_label.pack()
+total_sales_count_label=Label(sales_frame, text= '0', bg='#E74C3C', fg='white', font=('times new roman', '30', 'bold'))
+total_sales_count_label.pack(pady=10)
+
+
+
 
 
 window.mainloop()
